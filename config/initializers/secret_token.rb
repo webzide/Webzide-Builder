9af91070a71dfd44f4bd0bf40ad6ba340b927043
Dennis <dennis_dx@yahoo.com>
2011-01-03 12:32:10 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webzide11::Application.config.secret_token = '599bcdfe5c6a97681b0e5fbbf3bc7e6f959cf45feb164db6ba79d09e565bd942edf0f15098d86fb601cbbbecca62d5747e9dc40f18f08844b110f5cd098c5b09'
