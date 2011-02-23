68d77a63bf653e1411f9382739b6151dd4f4927d
Dennis <dennis_dx@yahoo.com>
2011-02-22 20:00:39 -0500

    feb22 12

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,15 @@
 class DashboardController < ApplicationController
+  include SessionsHelper
 
   layout 'application'
 
   def index
-
+    if signed_in?
+      @user=current_user
+    else
+      redirect_to login_path
+    end
+    @zide=Zide.find_by_user_id(current_user[:id])
   end
 
 end
