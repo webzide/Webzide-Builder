a1993b33780caf9e4a6d214f820cb29aa6febf1a
Dennis <dennis_dx@yahoo.com>
2011-02-22 20:06:50 -0500

    feb22 14

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,7 +6,7 @@ class DashboardController < ApplicationController
     if signed_in?
       @user=current_user
     else
-      redirect_to login_path
+      
     end
     @zide=Zide.find_by_user_id(current_user[:id])
   end
