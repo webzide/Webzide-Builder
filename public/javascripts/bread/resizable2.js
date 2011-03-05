1277f617fa045944f1853979c308775f7acd388f
Dennis <dennis_dx@yahoo.com>
2011-03-05 14:26:27 -0500

    march 5 27

--- public/javascripts/bread/resizable2.js
+++ public/javascripts/bread/resizable2.js
@@ -17,22 +17,55 @@ $(document).bind('ready', function(){
 
 
 
+            $(rightResize).bind('mousedown', function(event){
+                var currPos=event.clientX;
+                var currElem=event.target;
+                var currLeft = $(currElem).parent().width();
+                var parentLeft=$(currElem).parent().offset().left;
 
+                $("#page").bind('mousemove', function(event){
+                    $(currElem).parent().children('.verticalResize').width($(currElem).parent().width() - 10)
 
-                $(this).append($(rightResize));
+                   var nextPos=event.clientX;
+                   var resizedLength= nextPos - currPos;
+
+                   if($(currElem).offset().left < parentLeft){
+
+                    $(currElem).css("left", 0)
+                   } else{
+                    $(currElem).css("left", currLeft + resizedLength)
+
+                   }
+
+                   $(currElem).parent().css("width", nextPos - parentLeft);
+
+                })
+
+                $('#page').bind('mouseup', function(){
+                    if($(currElem).offset().left < parentLeft){
+                         $(currElem).parent().css('width', "0px")
+                         $(currElem).css("left", 0)
+                     }
+                     $(currElem).css('left', $(currElem).parent().width())
+                    $('#page').unbind('mousemove')
+
+                })
+
+            })
+
+            $(this).append(rightResize)
 
                 $('.rightResize').css({
-                    'position': 'absolute',
-                    'cursor': 'e-resize',
-                    'width': "4px",
-
-                    'top': '0px',
-                    'background-color': 'transparent'
+                    position: 'absolute',
+                    cursor: 'e-resize',
+                    width: "4px",
+                    height: $(this).height() - 10,
+                    left: $(this).width() - 2,
+                    top: '0px'
 
                 })
             }
 
-            //vertical resize
             
 
             //corner Resize
