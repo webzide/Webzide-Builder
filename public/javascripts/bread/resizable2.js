32805fa1982bdb8ededb47461c248a4a6c453208
Dennis <dennis_dx@yahoo.com>
2011-03-05 13:46:00 -0500

    march 5 5

--- public/javascripts/bread/resizable2.js
+++ public/javascripts/bread/resizable2.js
@@ -10,7 +10,34 @@ $(document).bind('ready', function(){
 
         $('.div').not('#page').each(function(){
             //right resize
-            
+            {
+            var rightResize = document.createElement('div');
+
+            $(rightResize).addClass('rightResize')
+
+            $(rightResize).css({
+                position: 'absolute',
+                cursor: 'e-resize',
+                'background-color': 'none',
+                width: "4px",
+                height: $(this).height() - 10,
+                left: $(this).width() - 2,
+                top: '0px'
+
+            })
+
+            $(rightResize).bind('mousedown', function(event){
+                var currPos=event.clientX;
+                var currElem=event.target;
+                var currLeft = $(currElem).parent().width();
+                var parentLeft=$(currElem).parent().offset().left;
+
+                
+
+            })
+
+            $(this).append(rightResize)
+            }
 
         })
 
